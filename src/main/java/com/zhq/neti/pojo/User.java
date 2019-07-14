ef8a376cba689872df92c62f974638b806cec859
zhengquan <zheng_quan45@163.com>
2019-07-14 23:33:24 +0800

    no comment

--- src/main/java/com/zhq/neti/pojo/User.java
+++ src/main/java/com/zhq/neti/pojo/User.java
@@ -1,6 +1,7 @@
 package com.zhq.neti.pojo;
 
 import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import java.util.Date;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @TableId(type = IdType.ID_WORKER)
