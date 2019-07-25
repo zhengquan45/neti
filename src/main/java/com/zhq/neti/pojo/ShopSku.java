82e340984d9eb4ce684e0ca799eb4165c6b17b82
zhengquan <zheng_quan45@163.com>
2019-07-25 12:42:48 +0800

    shop module

---
+++ src/main/java/com/zhq/neti/pojo/ShopSku.java
@@ -0,0 +1,23 @@
+package com.zhq.neti.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zhengquan
+ * @date 2019/7/24
+ */
+@TableName("t_shop_sku")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShopSku {
+    private Long wareHouseId;
+    private Long skuId;
+    private Integer num;
+    private String unit;
+}
