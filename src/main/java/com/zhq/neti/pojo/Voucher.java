9aa25cfccb29bef5d12e111d16064aa528dd7c9e
zhengquan <zheng_quan45@163.com>
2019-07-26 08:59:47 +0800

    no comment

---
+++ src/main/java/com/zhq/neti/pojo/Voucher.java
@@ -0,0 +1,30 @@
+package com.zhq.neti.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@TableName("t_voucher")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Voucher {
+    @TableId(type = IdType.ID_WORKER)
+    private Long id;
+    private BigDecimal deno;
+    private BigDecimal condition;
+    private Date startDate;
+    private Date endDate;
+    private Integer maxNum;
+    @TableLogic
+    private Boolean isDeleted;
+}
